002917ad0748c34d9a70ba2c542f2b91450acbf6
Ericke <97632185+erickepereira@users.noreply.github.com>
2022-08-12 16:52:55 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da pagina do organo" />
+        </header>
+
+    )
+}
+
+export default Banner
